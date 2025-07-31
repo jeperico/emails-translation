4fadf48d3baaa2f119db268fea5c15359cb575f3
jeperico <joaoeperico@gmail.com>
2025-07-31 16:35:29 -0300

    feat: list current containers id

---
+++ containers.py
@@ -0,0 +1,12 @@
+from conn import client
+
+def list_containers():
+  _, stdout, stderr = client.exec_command('sudo ls -l /var/lib/docker/containers')
+
+  output = stdout.read().decode().strip()
+  error = stderr.read().decode().strip()
+
+  if error:
+    print("❌ Error:", error)
+  else:
+    print("✅ Output:", output)
