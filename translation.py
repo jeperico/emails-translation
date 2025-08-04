351ed83bf87c17c6151e606f8b8eaba91e110272
jeperico <joaoeperico@gmail.com>
2025-08-04 14:26:23 -0300

    feat: finish translation module

--- translation.py
+++ translation.py
@@ -1,22 +1,7 @@
-from googletrans import Translator
+from translate import Translator
 
 def translate(text: str):
-  google = Translator()
-  result = google.translate(text, dest='pt')
-  print(result.text)
-
-# TODO: Handle variables name to don't translate
-# TODO: Transform into a exportable function
-# TODO: Format the response
-if __name__ == "__main__":
-  translate(
-    '''<p>Hi {{user.available_name}},</p>
-
-<p>
-  Conversation #{{conversation.display_id}} in {{ inbox.name }} 
-  has missed the SLA for resolution time under policy {{ sla_policy.name }}.
-</p>
-
-<p>
-<a href="{{action_url}}">Please address immediately.</a>
-</p''')
+  translatorPt = Translator(to_lang="pt-br")
+  result = translatorPt.translate(text)
+  
+  return result
